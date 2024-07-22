3378db82dfe289081627282d4b7b5ca8fa178e41
TiagoShop <melbraeisantiago@gmail.com>
2024-07-22 09:37:16 +0800

    add

--- server/src/index.js
+++ server/src/index.js
@@ -33,7 +33,7 @@ const upload = multer({ storage: storage });
 app.use(express.json());
 app.use(cors({
   origin: 'https://tiagoshop.vercel.app',
-  methods: ['GET', 'POST', 'PATCH', 'DELETE']
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'ADD']
 }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
