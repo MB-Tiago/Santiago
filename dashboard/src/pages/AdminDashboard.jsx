d10d155d226a85774a148cf2f226cdc1bdf543da
TiagoShop <melbraeisantiago@gmail.com>
2024-07-23 10:20:43 +0800

    asdfsdf

--- dashboard/src/pages/AdminDashboard.jsx
+++ dashboard/src/pages/AdminDashboard.jsx
@@ -112,7 +112,7 @@ const AdminDashboard = () => {
     
             const cloudinaryData = new FormData();
             cloudinaryData.append('file', productImage);
-            cloudinaryData.append('upload_preset', 'dqh9de7m'); // Adjust as necessary
+            cloudinaryData.append('upload_preset', 'ml_default'); // Adjust as necessary
     
             console.log('Sending request to Cloudinary');
             const cloudinaryResponse = await axios.post(
--- server/src/index.js
+++ server/src/index.js
@@ -246,7 +246,7 @@ app.post('/addproduct', async (req, res) => {
       name: productName,
       description: productDescription,
       price: productPrice,
-      image: imageUrl // Use the Cloudinary URL
+      image: imageUrl
     });
 
     const savedProduct = await newProduct.save();
